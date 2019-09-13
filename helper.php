00da57d94847fa4e850175aa781569585d108c1d
Carlos Cámara <carlos@hepta.es>
2019-09-13 06:21:44 +0200

    Added relations

--- helper.php
+++ helper.php
@@ -16,33 +16,64 @@ JModelLegacy::addIncludePath(JPATH_SITE . '/components/com_content/models', 'Con
 
 class modarticlesthumbnailsHelper
 {
-	static function getItems($params)
+	/**
+	 * Get the articles to show
+	 *
+	 * @param	object	$params	Module parameters
+	 *
+	 * @return	array	Array of items
+	 */
+	static public function getItems($params)
 	{
 		// Set application parameters in model
 		$app       = JFactory::getApplication();
 		$appParams = $app->getParams();
-		
+
 		$option = $app->input->get('option');
-    	$view   = $app->input->get('view');
-		
+		$view   = $app->input->get('view');
+
 		$onlyInArticles = $params->get('show_only_in_articles');
-		
+
 		$relatedArticles = true;
 
-        if ($onlyInArticles)
-        {
-    		$option = $app->input->get('option');
-    		$view   = $app->input->get('view');
-            
-            if (!($option === 'com_content' && $view === 'article'))
-    		{
-    			return array();
-    		}
+		if ($onlyInArticles)
+		{
+			$option = $app->input->get('option');
+			$view   = $app->input->get('view');
+
+			if (!($option === 'com_content' && $view === 'article'))
+			{
+				return array();
+			}
+
+			$temp = $app->input->getString('id');
+			$temp = explode(':', $temp);
+			$id   = $temp[0];
 		}
-		
+
 		// Get an instance of the generic articles model
 		$model     = JModelLegacy::getInstance('Articles', 'ContentModel', array('ignore_request' => true));
-		
+
+		switch ($params->get('related_mode', 'false'))
+		{
+			case 'category':
+				$categoryId = self::getArticleCategory($id);
+				$model->setState('filter.category_id', $categoryId);
+				break;
+			case 'tags':
+				break;
+			case 'keywords':
+				$articleIds = self::getByKeyword($id);
+
+				if (count($articleIds))
+				{
+					$model->setState('filter.article_id', $articleIds);
+				}
+				break;
+			case 'title':
+				break;
+		}
+
 		// Set application parameters in model
 		$app       = JFactory::getApplication();
 		$appParams = $app->getParams();
@@ -60,23 +91,27 @@ class modarticlesthumbnailsHelper
 		$model->setState('filter.access', $access);
 
 		// Category filter
-		$model->setState('filter.category_id', $params->get('catid', array()));
-		
+		if (!isset($categoryId))
+		{
+			$model->setState('filter.category_id', $params->get('catid', array()));
+		}
+
 		// Filter by language
 		$model->setState('filter.language', $app->getLanguageFilter());
 
 		// Ordering
-		if($params->get('ordering') == 'random')
-        {
-            $model->setState('list.ordering', JFactory::getDbo()->getQuery(true)->Rand());
-        }
-        else
-        {
-            $model->setState('list.ordering', 'a.'.$params->get('ordering', 'publish_up'));
-            $model->setState('list.direction', $params->get('direction', 'DESC'));
-        }
+		if ($params->get('ordering') == 'random')
+		{
+			$model->setState('list.ordering', JFactory::getDbo()->getQuery(true)->Rand());
+		}
+		else
+		{
+			$model->setState('list.ordering', 'a.' . $params->get('ordering', 'publish_up'));
+			$model->setState('list.direction', $params->get('direction', 'DESC'));
+		}
 
 		$items = $model->getItems();
+
 		foreach ($items as &$item)
 		{
 			$item->slug    = $item->id . ':' . $item->alias;
@@ -94,5 +129,128 @@ class modarticlesthumbnailsHelper
 		}
 
 		return $items;
+	}
+
+	/**
+	 * Get current Article Category
+	 *
+	 * @param	integer	$id	Article id
+	 *
+	 * @return	integer	Category id
+	 */
+	public static function getArticleCategory($id)
+	{
+		$db = JFactory::getDbo();
+
+		$query = $db->getQuery(true);
+
+		// Select the meta keywords from the item
+		$query->select('catid')
+			->from('#__content')
+			->where('id = ' . (int) $id);
+		$db->setQuery($query);
+
+		try
+		{
+			$catId = $db->loadResult();
+		}
+		catch (RuntimeException $e)
+		{
+			JFactory::getApplication()->enqueueMessage(JText::_('MOD_ARTICLE_THUMBNAILS_AN_ERROR_HAS_OCCURRED'), 'error');
+
+			$catId = 0;
+		}
+
+		return $catId;
+
+	}
+
+	/**
+	 * Get related articles by Keyword
+	 *
+	 * @param	integer	$id	Article id
+	 *
+	 * @return	array	Array of articles
+	 */
+	public static function getByKeyword($id)
+	{
+		$db = JFactory::getDbo();
+
+		$query = $db->getQuery(true);
+
+		// Select the meta keywords from the item
+		$query->select('metakey')
+			->from('#__content')
+			->where('id = ' . (int) $id);
+		$db->setQuery($query);
+
+		try
+		{
+			$metakey = trim($db->loadResult());
+		}
+		catch (RuntimeException $e)
+		{
+			JFactory::getApplication()->enqueueMessage(JText::_('MOD_ARTICLE_THUMBNAILS_AN_ERROR_HAS_OCCURRED'), 'error');
+
+			return array();
+		}
+
+		// Explode the meta keys on a comma
+		$keys  = explode(',', $metakey);
+		$likes = array();
+
+		// Assemble any non-blank word(s)
+		foreach ($keys as $key)
+		{
+			$key = trim($key);
+
+			if ($key)
+			{
+				$likes[] = $db->escape($key);
+			}
+		}
+
+		if (count($likes))
+		{
+			// Select other items based on the metakey field 'like' the keys found
+			$query->clear()
+				->select('a.id')
+				->from('#__content AS a')
+				->where('a.id != ' . (int) $id)
+				->where('a.state = 1')
+				->where('a.access IN (' . $groups . ')');
+
+			$wheres = array();
+
+			foreach ($likes as $keyword)
+			{
+				$wheres[] = 'a.metakey LIKE ' . $db->quote('%' . $keyword . '%');
+			}
+
+			$query->where('(' . implode(' OR ', $wheres) . ')')
+				->where('(a.publish_up = ' . $db->quote($nullDate) . ' OR a.publish_up <= ' . $db->quote($now) . ')')
+				->where('(a.publish_down = ' . $db->quote($nullDate) . ' OR a.publish_down >= ' . $db->quote($now) . ')');
+
+			// Filter by language
+			if (JLanguageMultilang::isEnabled())
+			{
+				$query->where('a.language in (' . $db->quote(JFactory::getLanguage()->getTag()) . ',' . $db->quote('*') . ')');
+			}
+
+			$db->setQuery($query, 0, $maximum);
+
+			try
+			{
+				$articleIds = $db->loadColumn();
+			}
+			catch (RuntimeException $e)
+			{
+				JFactory::getApplication()->enqueueMessage(JText::_('JERROR_AN_ERROR_HAS_OCCURRED'), 'error');
+
+				return array();
+			}
+
+			return $articleIds;
+		}
 	}
 }
